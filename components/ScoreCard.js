18b1f49c7f956fcc1906bb03c21248ab08353f81
SayarB <sayarbhattacharyya03@gmail.com>
2021-07-31 11:16:36 +0530

    Home button linking to the home page added to score card

--- components/ScoreCard.js
+++ components/ScoreCard.js
@@ -1,12 +1,15 @@
 import styles from "../styles/Quiz.module.css";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Button from "@material-ui/core/Button";
+import { useRouter } from "next/router";
 export default function ScoreCard({
   totalQuestions,
   score,
   resetQuiz,
   retakeQuiz,
 }) {
+  const router = useRouter();
+
   return (
     <div className={styles.quiz_card}>
       <h1>
@@ -38,6 +41,15 @@ export default function ScoreCard({
           variant="outlined"
         >
           Reset
+        </Button>
+        <Button
+          onClick={() => {
+            router.push("/");
+          }}
+          color="primary"
+          variant="outlined"
+        >
+          Home
         </Button>
       </div>
     </div>
